75bf803af0189c4c92fca0c0c18040c1492f26a3
kingwkb <1519969+kingwkb@users.noreply.github.com>
2025-12-30 16:52:08 -0700

    Fix syntax for macOS dependency and clean up whitespace in PinentrySwift formula

--- Formula/pinentry-swift.rb
+++ Formula/pinentry-swift.rb
@@ -4,7 +4,7 @@ class PinentrySwift < Formula
   url "https://github.com/kingwkb/pinentry-swift/releases/download/v0.0.2/pinentry-swift-v0.0.2.tar.gz"
   sha256 "449fc121bd9f342a291f40620a103448010248b42b54020de4c00237411c5dd4"
   license "MIT"
-  depends_on :macos => :monterey
+  depends_on macos: :monterey
 
   def install
     bin.install "pinentry-swift"
@@ -13,7 +13,7 @@ class PinentrySwift < Formula
   def caveats
     <<~EOS
       Installation successful!
-      
+
       To enable this pinentry, ensure your ~/.gnupg/gpg-agent.conf contains:
         pinentry-program #{HOMEBREW_PREFIX}/bin/pinentry-swift
 
